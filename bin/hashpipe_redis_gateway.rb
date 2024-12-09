43a8d61b0ea6b11cb9e2de526566bd491ad21c2d
radonnachie <code@radonn.co.za>
2024-12-09 17:30:39 +0000

    *= bin redis_gateway LoC reorder

--- bin/hashpipe_redis_gateway.rb
+++ bin/hashpipe_redis_gateway.rb
@@ -503,9 +503,10 @@ def update_redis(redis, instance_ids, notify=false)
   instance_ids.each do |iid|
     # redis key for this instance's status buffer
     key = "#{OPTS[:domain]}://#{OPTS[:gwname]}/#{iid}/status"
+    existing_redis_status_keys = redis.hkeys(key).to_set
+
     sb = STATUS_BUFS[iid]
     sb_hash = sb.to_hash
-    existing_redis_status_keys = redis.hkeys(key).to_set
     
     redis.pipelined do |pipeline|
       # Each status buffer update happens in a transaction
